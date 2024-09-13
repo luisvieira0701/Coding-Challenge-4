1ddfde366e78bf74cbcf95bdb95a4cb350bda301
LuisVieira0701 <luisvieira@usf.edu>
2024-09-13 18:46:12 -0400

    Task - 1 Create inventory array of product objects

---
+++ inventory_management.js
@@ -0,0 +1,13 @@
+// Initialize inventory with product objects
+const inventory = [
+    {name: 'Basketball', price: 150, quantity: 50, lowStockLevel: 10},
+    {name: 'Soccerball', price: 120, quantity: 35, lowStockLevel: 5},
+    {name: 'Baseball', price: 50, quantity: 70, lowStockLevel: 15},
+    {name: 'Tennisball', price: 30, quantity: 100, lowStockLevel: 40},
+    {name: 'Pickleball', price: 15, quantity: 60, lowStockLevel: 50}
+];
+
+
+
+
+
